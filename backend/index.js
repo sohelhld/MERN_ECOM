e7ed9a4e00eb4a3d208c28375aee920cf6a5280c
Sourav Sarkar <souravsantipur9635@gmail.com>
2024-02-08 01:15:40 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
 const express = require('express')
-var cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser')
 const { connected } = require('./db')
 const { productRoute } = require('./routes/product.route')
 const { userRouter } = require('./routes/user.route')
 const { orderRouter } = require('./routes/orderRouter')
-
+const cors = require('cors')
 require("dotenv").config()
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors())
 
 app.use("/users",userRouter)
 app.use("/product",productRoute)
