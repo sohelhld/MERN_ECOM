3e8ab37cb6d96e1889ca61e9067a312957fe7a24
sohelhld <haldersohelrana@gmail.com>
2024-04-13 00:13:38 +0530

    auth

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -3,42 +3,39 @@ const { userModel } = require("../models/userModel");
 require("dotenv").config();
 
 const Auth = async (req, res, next) => {
-  try {
-    const  token  = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    try {
+        const token =
+            req.cookies.token || req.headers.authorization?.split(" ")[1];
 
-    
-// console.log(token);
-    if (!token)
-      return res.status(401).send({ message: "user is not authinticated " });
+        // console.log(token);
+        if (!token)
+            return res
+                .status(401)
+                .send({ message: "user is not authinticated " });
 
-    const isTokenCorrect = await jwt.verify(token, process.env.jwt_secret);
-    if (!isTokenCorrect)
-      return res.status(401).send({ message: "token is not correct" });
-    req.body.email = isTokenCorrect.email;
-    req.body.name = isTokenCorrect.name;
-    req.body.userId = isTokenCorrect.userid;
-    req.user = await userModel.findById(isTokenCorrect.id);
+        const isTokenCorrect = await jwt.verify(token, process.env.jwt_secret);
+        if (!isTokenCorrect)
+            return res.status(401).send({ message: "token is not correct" });
+        req.body.email = isTokenCorrect.email;
+        req.body.name = isTokenCorrect.name;
+        req.body.userId = isTokenCorrect.userid;
+        req.user = await userModel.findById(isTokenCorrect.id);
 
-    next();
-  } catch (error) {
-    res.status(404).send(error.message);
-  }
+        next();
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
 };
 
 const authorizeRoles = (...roles) => {
     return (req, res, next) => {
-      if (!roles.includes(req.user.role)) {
-        return res
-          .status(401)
-          .send({
-            message: `Role:${req.user.role} is not allowed to access this resource`,
-          });
-      }
-      next();
+        if (!roles.includes(req.user.role)) {
+            return res.status(401).send({
+                message: `Role:${req.user.role} is not allowed to access this resource`,
+            });
+        }
+        next();
     };
-  };
+};
 
-
-module.exports = { Auth ,authorizeRoles};
-
-
+module.exports = { Auth, authorizeRoles };
